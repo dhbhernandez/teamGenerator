b9d4555376814c3ad501740bb21dd9cb659e0c20
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2020-12-03 01:20:37 -0600

    readme file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# teamGenerator
+# Team Profile Generator  
+![APM](https://img.shields.io/apm/l/pack)
+## Description
+    I want to generate a profile webpage that displays my team's basic info
+## Table of Contents
+    
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+* [Tests](#tests)
+* [Contributing](#contributing)
+* [Description](#description)
+            
+## Installation
+    ruin the app on node.js.
+## Usage
+    In your GitBash terminal run the command "node app.js". answer the questions, to create profiles for team members. 
+
+## Credits
+    N/A
+## License
+    MIT
+## Contributing
+    N/A
+## Tests
+    Run test in the terminal using npm run test. Make sure to install the jest module from npm.
+## Questions
+Any questions or suggestions reach:
+* Diego Hernandez
+* Via [Github] https://github.com/dhbhernandez
+* Via Email: d8328827238@me.com
--- app.js
+++ app.js
@@ -6,13 +6,11 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
 let teamMembers = [];
 
 const firstQuestion = {
@@ -28,7 +26,11 @@ const questionsYourRole = {
     choices: ["Intern", "Engineer", "Manager"],
 };
 
-// node 
+const fileNameQuestion = {
+    type: "input",
+    message: "Please enter valid file name",
+    name: "fileName",
+};
 
 const Questions = {
     Manager: [
@@ -99,7 +101,6 @@ const Questions = {
     ],
 };
 
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -114,7 +115,7 @@ const addOrFinish = () => {
             selectRole();
         } else {
             //generateTeam();
-            // getFileName();
+            getFileName();
         }
     });
 };
@@ -150,36 +151,46 @@ const roleQuestions = (questions, role) => {
         addOrFinish();
     });
 };
+console.log(teamMembers);
 
+const getFileName = () => {
+    inquirer.prompt(fileNameQuestion).then((answer) => {
+        if (answer.fileName) {
+            generateTeam(answer.fileName);
+        } else {
+            getFileName();
+        }
+    });
+};
 
-// const isFileNameValid = (fileName) => {
-//   if(fileName === )
-// }
+const generateTeam = (fileName) => {
+    const outputPath = path.join(OUTPUT_DIR, fileName + ".html");
+
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+
+    fs.writeFileSync(outputPath, render(teamMembers), (err) => {
+        if (err) {
+            console.log(err);
+            getFileName();
+        }
+    });
+};
 
 startApp();
-
-
 // After you have your html, you're now ready to create an HTML file using the HTML
-
 // returned from the `render` function. Now write it to a file named `team.html` in the
-
 // `output` folder. You can use the variable `outputPath` above target this location.
-
 // Hint: you may need to check if the `output` folder exists and create it if it
-
 // does not.
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
-
 // information; write your code to ask different questions via inquirer depending on
-
 // employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-
 // and Intern classes should all extend from a class named Employee; see the directions
-
 // for further information. Be sure to test out each class and verify it generates an
-
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work! ``
